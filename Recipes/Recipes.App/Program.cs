afd0b5eec6972700390d706c802ae5ded6a627d8
David Störmer <badf00d@posteo.de>
2017-12-22 19:13:48 +0100

    Added not working Bootstrapper

--- Recipes/Recipes.App/Program.cs
+++ Recipes/Recipes.App/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Nancy;
 
 namespace Recipes.App
 {
